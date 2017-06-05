fb86fa716d2dcd36666f03a2a91a824f73a13a6c
xxBei <zzw_bei@163.com>
2017-06-05 11:40:13 +0800

    weather数据库

---
+++ weather.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2017-06-05 11:32:18
+-- 服务器版本： 5.7.14
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `weather`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `db_manager`
+--
+
+CREATE TABLE `db_manager` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `db_manager`
+--
+
+INSERT INTO `db_manager` (`id`, `username`, `password`) VALUES
+(1, 'zzw', '123456'),
+(2, 'zzw001', '123456'),
+(3, 'zzw002', '123456'),
+(4, 'zzw003', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `db_manager`
+--
+ALTER TABLE `db_manager`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `db_manager`
+--
+ALTER TABLE `db_manager`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
